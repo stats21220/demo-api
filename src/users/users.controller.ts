2bc80ce06b2eed4742b68fc62521e452af892330
Станислав Афанасьев <stanislavafanasev@MacBook-Air-Stanislav.local>
2021-12-27 15:42:50 +0300

    091 guard авторизация

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { ValidateMiddleware } from '../common/validate.middleware'
 import { sign } from 'jsonwebtoken'
 import { IConfigService } from '../config/config.service.interface'
 import { IUserService } from './users.service.interface'
+import { AuthGuard } from '../common/auth.guard'
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -38,7 +39,7 @@ export class UserController extends BaseController implements IUserController {
 				path: '/info',
 				method: 'get',
 				func: this.info,
-				middlewares: [],
+				middlewares: [new AuthGuard()],
 			},
 		])
 	}
@@ -67,8 +68,9 @@ export class UserController extends BaseController implements IUserController {
 		}
 		this.ok(res, { id: result.id, email: result.email })
 	}
-	async info({ user }: Request, res: Response, next: NextFunction) {
-		this.ok(res, { email: user })
+	async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
+		const userInfo = await this.userService.getUserInfo(user)
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id })
 	}
 	private signJWT(email: string, secret: string): Promise<string> {
 		return new Promise<string>((resolve, reject) => {
